e3cf331d7028fd6301220a06b3d465eab0171bb7
MiaDHayes <miadeyon@outlook.com>
2024-03-15 19:10:29 -0500

    main, app, login, and create account completed

---
+++ Minutes/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import Nav from "./Nav"
+
+function Header() {
+
+    return (
+        <div>
+            <h5> Minutes </h5>
+            <Nav />
+        </div>
+    )
+}
+
+export default Header
